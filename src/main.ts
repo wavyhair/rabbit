24da07163e243faf76cea8a2f23f4aea580d852a
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-09-06 20:46:54 +0800

    💄 UI: add normalize.css

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@
  * @Author: CHENJIE
  * @Date: 2022-09-06 18:46:37
  * @LastEditors: CHENJIE
- * @LastEditTime: 2022-09-06 20:26:41
+ * @LastEditTime: 2022-09-06 20:45:16
  * @FilePath: \rabbit-ts-vue3\src\main.ts
  * @Description:main
  */
 import { createApp } from 'vue'
 import './style.css'
+import 'normalize.css'
 import App from './App.vue'
 import router from './router'
 const app = createApp(App)
