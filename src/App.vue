5b888cb69a91c0f8e8b6bea66c30d42f32e6cffe
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-09-06 19:53:21 +0800

    🔧 Config: vsCode Eslint Config

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,11 @@
  * @Author: chenjie
  * @Date: 2022-09-06 11:38:25
  * @LastEditors: CHENJIE
- * @LastEditTime: 2022-09-06 19:25:14
+ * @LastEditTime: 2022-09-06 19:45:18
  * @FilePath: \rabbit-ts-vue3\src\App.vue
  * @Description: App
 -->
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="app">App</div>
@@ -16,6 +15,6 @@
 <style lang="less" scoped>
 .app {
   color: @xtxColor;
-  .hoverShadow()
+  .hoverShadow();
 }
 </style>
