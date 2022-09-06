1b80dafd3373318d8611c43ae875c4ae6a2e960a
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-09-06 21:44:06 +0800

    💄 UI: add common.less

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@
  * @Author: CHENJIE
  * @Date: 2022-09-06 18:46:37
  * @LastEditors: CHENJIE
- * @LastEditTime: 2022-09-06 20:45:16
+ * @LastEditTime: 2022-09-06 20:55:24
  * @FilePath: \rabbit-ts-vue3\src\main.ts
  * @Description:main
  */
 import { createApp } from 'vue'
 import './style.css'
 import 'normalize.css'
+import '@/assets/styles/common.less'
 import App from './App.vue'
 import router from './router'
 const app = createApp(App)
