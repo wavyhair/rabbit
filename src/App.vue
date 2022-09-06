b45ad167b81ad4c3f47e4068372aa55e1b493050
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-09-06 19:17:18 +0800

    🍻 Beer: config alias

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <!--
  * @Author: chenjie
  * @Date: 2022-09-06 11:38:25
- * @LastEditors: chenjie
- * @LastEditTime: 2022-09-06 13:11:20
- * @FilePath: /src/App.vue
+ * @LastEditors: CHENJIE
+ * @LastEditTime: 2022-09-06 19:15:45
+ * @FilePath: \rabbit-ts-vue3\src\App.vue
  * @Description: App
 -->
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/*
+ * @Author: CHENJIE
+ * @Date: 2022-09-06 18:46:37
+ * @LastEditors: CHENJIE
+ * @LastEditTime: 2022-09-06 19:15:16
+ * @FilePath: \rabbit-ts-vue3\vite.config.ts
+ * @Description: vite.config
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
 })
