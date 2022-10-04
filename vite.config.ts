f3927a28369ea9815f6de7f5147c8b5428299448
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-10-04 23:03:04 +0800

    🔧 Config: update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: CHENJIE
  * @Date: 2022-09-06 18:46:37
  * @LastEditors: CHENJIE
- * @LastEditTime: 2022-09-12 11:19:59
+ * @LastEditTime: 2022-10-04 23:00:07
  * @FilePath: \rabbit-ts-vue3\vite.config.ts
  * @Description: vite.config
  */
@@ -19,6 +19,14 @@ export default defineConfig({
     }),
     vueSetupExtend(),
   ],
+  server: {
+    // QQ三方登录的回调uri为：http://www.corho.com:8080/#/login/callback
+    // vite 中配置： www.corho.com:8080
+    host: 'www.corho.com',
+    port: 8080,
+    open: true,
+    cors: true,
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
