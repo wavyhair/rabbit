d7f5daa7899dc760eb35bf1ca6290aab44e33701
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-10-15 15:20:33 +0800

    💩 Poo: 错误注释

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: CHENJIE
  * @Date: 2022-09-06 18:46:37
  * @LastEditors: CHENJIE
- * @LastEditTime: 2022-10-15 15:11:05
+ * @LastEditTime: 2022-10-15 15:19:21
  * @FilePath: \rabbit-ts-vue3\src\main.ts
  * @Description:main
  */
@@ -13,7 +13,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
-import XtxUi from '@/components/index' // 全局组件
+import XtxUi from '@/components/index' // 全局组件  👺  使用 unplugin-vue-components 插件之后会在 src 下生成 components.d.ts文件 默认引入 .d.ts文件之后就会报错 所以这引入路径需要写完整 否则 app.use()时候会报类型错误
 import directives from './directives' // 自定义指令
 
 const app = createApp(App)
